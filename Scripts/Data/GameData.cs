3fec8166182e2c459362d07d1df618ff232f8837
Guendeli <guendeliomar@gmail.com>
2022-08-21 20:56:02 +0400

    .

--- Scripts/Data/GameData.cs
+++ Scripts/Data/GameData.cs
@@ -18,4 +18,21 @@ namespace Game
         AntiClockwise,
         Shoot
     }
+
+
+    [Serializable]
+    public class ShipModel
+    {
+        public int ShipId = 0;                  // Id of the ship, we can have multiples (Events ? Gacha ? IAP ? Unlockable)
+        public int HP = 100;
+        public float Acceleration = 30f;
+        public float MaxAcceleration = 60f;
+        public float Torque;
+        public float MaxTorque;
+        public float Drag;                  // how much the ship decelerates each tick
+        public float AngularDrag;           // same as above but for torque
+        public int Attack;
+        public int RateOfFire;              // how many ticks between each fire
+        public string PrefabRootName;       // in case designers want full control over visuals
+    }
 }
